7846fc7fd5760edd4a4208a422b76bb49741614b
Philip Double <philip.double@gmail.com>
2020-06-26 09:44:31 -0400

    updating config for graphql lint and mock support

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  pluginOptions: {
+    apollo: {
+      lintGQL: true,
+      enableMocks: true,
+      enableEnging: true,
+    },
+  },
 };
